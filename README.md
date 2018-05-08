48dd85b81fff35e698bdda217b21bd4b28de8dd4
Yixiong <yixiong@kesci.com>
2018-05-08 10:44:39 +0800

    add script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# -
+# 快手用户兴趣建模大赛 --- 评审环节
+欢迎参加科赛网与快手联合举办的用户兴趣建模大赛。经授权，我们开放此次比赛的评审脚本，供参赛选手在本地环境对个人的模型做验证(validation)。
+
+## 测评指标
+此次比赛用到的测评方式为**AUC**，具体详见[Wikipedia](https://en.wikipedia.org/wiki/Receiver_operating_characteristic#Area_under_the_curve)
+
+## 使用方法
+* `git clone`仓库到本地
+* 运行`python evaluation_script.py submit_file test_file`,submit_file为你的提交，test_file为答案集合
---
+++ evaluation_script.py
@@ -0,0 +1,89 @@
+import os, sys
+import numpy as np
+import pandas as pd
+
+def scoreAUC(labels, probs):
+    i_sorted = sorted(range(len(probs)),key=lambda i: probs[i],
+                      reverse=True)
+    auc_temp = 0.0
+    TP = 0.0
+    TP_pre = 0.0
+    FP = 0.0
+    FP_pre = 0.0
+    P = 0
+    N = 0
+    last_prob = probs[i_sorted[0]] + 1.0
+    for i in range(len(probs)):
+        if last_prob != probs[i_sorted[i]]: 
+            auc_temp += (TP+TP_pre) * (FP-FP_pre) / 2.0        
+            TP_pre = TP
+            FP_pre = FP
+            last_prob = probs[i_sorted[i]]
+        if labels[i_sorted[i]] == 1:
+            TP = TP + 1
+        else:
+            FP = FP + 1
+    auc_temp += (TP+TP_pre) * (FP-FP_pre) / 2.0
+    auc = auc_temp / (TP * FP)
+    return auc
+
+def validate(submit_file, test_file, options = True):
+
+    lines = open(test_file).readlines()
+    data = []
+    for line in lines:
+        s = line.strip('\n').split('\t')
+        data.append(s[0] + '_' + s[1])
+        #data[s[1] + '_' + s[2]] = int(s[0])
+
+    lines = open(submit_file).readlines()
+    submit_data = []
+    for line in lines:
+        s = line.strip('\n').split('\t')
+        submit_data.append(s[0] + '_' + s[1])
+    
+    data.sort()
+    submit_data.sort()
+
+    if data == submit_data and len(data) == len(submit_data):
+
+        return {'code': 0, 'message': 'validation success'}
+    else:
+        return {'code': -1, 'message': 'validation fail'}
+
+def score(submit_file, test_file, options = True):
+
+    lines = open(test_file).readlines()
+    data = {}
+    for line in lines:
+        s = line.strip('\n').split('\t')
+        #data.append(s[1] + '_' + s[2])
+        data[s[0] + '_' + s[1]] = float(s[2])
+
+    lines = open(submit_file).readlines()
+    submit_data = {}
+    for line in lines:
+        s = line.strip('\n').split('\t')
+        submit_data[s[0] + '_' + s[1]] = float(s[2])
+    
+    labels = []
+    probs = []
+    for key in data.keys():
+        labels.append(data[key])
+        probs.append(submit_data[key])
+
+    auc = scoreAUC(labels, probs)
+
+    return {
+        'code': 0, 
+        'score': auc,
+        'message': 'success'
+        }
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print ('usage: python score.py submit_file test_file')
+    submit_file = sys.argv[1]
+    test_file = sys.argv[2]
+    print (validate(submit_file, test_file))
+    print (score(submit_file, test_file))
